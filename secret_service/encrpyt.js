eddc8e9e401a61bc4c54a56259eb75a0702b8788
Bojan Djurkovic <dbojan@gmail.com>
2017-01-01 15:29:37 -0400

    code cleanup

--- secret_service/encrpyt.js
+++ secret_service/encrpyt.js
@@ -6,9 +6,7 @@ const chance = new Chance()
 module.exports = function (secret, iterations = 10) {
   const fakeError = chance.bool({ likelihood: 5 })
   if (fakeError) throw new Error('Error encrypting secret')
-  if (!secret || typeof secret !== 'string') {
-    throw new Error('Secret required')
-  }
+  if (!secret || typeof secret !== 'string') throw new Error('Secret required')
 
   return bcrypt.hash(secret, iterations)
 }
--- secret_service/save.js
+++ secret_service/save.js
@@ -8,13 +8,8 @@ const chance = new Chance()
 const DB_WRITE_FILE = 'secret_db_write.json'
 
 async function save (id, data) {
-  if (!id || typeof id !== 'string') {
-    throw new Error('ID required')
-  }
-
-  if (!data || typeof data !== 'string') {
-    throw new Error('Data required')
-  }
+  if (!id || typeof id !== 'string') throw new Error('ID required')
+  if (!data || typeof data !== 'string') throw new Error('Data required')
 
   const fakeError = chance.bool({ likelihood: 5 })
   if (fakeError) throw new Error('Error saving data')
