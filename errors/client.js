158e72e01a2c864d1815c9b6cd1d657456e68d4e
Bojan Djurkovic <dbojan@gmail.com>
2018-01-19 23:01:40 -0400

    added code for errors example

---
+++ errors/client.js
@@ -0,0 +1,170 @@
+const path = require('path')
+const async = require('async')
+const grpc = require('grpc')
+
+const PROTO_PATH = path.resolve(__dirname, '../protos/errorexample.proto')
+const HOSTPORT = '0.0.0.0:50051'
+
+const loaded = grpc.load(PROTO_PATH)
+const client = new loaded.ErrorExample.SampleService(HOSTPORT, grpc.credentials.createInsecure())
+
+function getWidgetOK (fn) {
+  client.getWidget({ id: 0 }, (err, data) => {
+    if (err) {
+      console.error('Error: %s', err)
+      return fn()
+    }
+    console.log(data)
+    fn()
+  })
+}
+
+function getWidgetError (fn) {
+  client.getWidget({ id: 4 }, (err, data) => {
+    if (err) {
+      console.error('Error: %s', err)
+      return fn()
+    }
+    console.log(data)
+    fn()
+  })
+}
+
+function listWidgets (fn) {
+  const call = client.listWidgets({ id: 3 })
+  call.on('data', d => {
+    console.log(d)
+  })
+  call.on('error', err => {
+    console.error('Error: %s', err)
+  })
+  call.on('end', fn)
+}
+
+function listWidgetsError (fn) {
+  const call = client.listWidgets({ id: 8 })
+  call.on('data', d => {
+    if (d.widget) {
+      console.log(d.widget)
+    } else if (d.error) {
+      console.log('Data error: %s', d.error.message)
+    }
+  })
+  call.on('error', err => {
+    console.error('Client error: %s', err)
+  })
+  call.on('end', fn)
+}
+
+function createWidgets (fn) {
+  const call = client.createWidgets((err, res) => {
+    if (err) {
+      console.error('Error: %s', err)
+      return fn()
+    }
+    console.log(res)
+  })
+
+  const widgets = [
+    { name: 'w1' },
+    { name: 'w2' },
+    { name: 'w3' }
+  ]
+
+  widgets.forEach(w => call.write(w))
+  call.end()
+}
+
+function createWidgetsError (fn) {
+  const call = client.createWidgets((err, res) => {
+    if (err) {
+      console.error('Error: %s', err)
+      return fn()
+    }
+    console.log(res)
+  })
+
+  const widgets = [
+    { name: 'w1' },
+    { name: 'w2' },
+    { name: 'w3' },
+    { name: 'w4' }
+  ]
+
+  widgets.forEach(w => call.write(w))
+  call.end()
+}
+
+function syncWidgets (fn) {
+  const call = client.syncWidgets()
+  call.on('data', d => {
+    if (d.widget) {
+      console.log(d.widget)
+    } else if (d.error) {
+      console.log('Data error: %s', d.error.message)
+    }
+  })
+  call.on('error', err => {
+    console.error('Client error: %s', err)
+  })
+  const widgets = [
+    { name: 'w1' },
+    { name: 'w2' },
+    { name: 'w3' }
+  ]
+
+  widgets.forEach(w => call.write({ widget: w }))
+  call.end()
+  fn()
+}
+
+function syncWidgetsError (fn) {
+  const call = client.syncWidgets()
+  call.on('data', d => {
+    if (d.widget) {
+      console.log(d.widget)
+    } else if (d.error) {
+      console.log('Data error: %s', d.error.message)
+    }
+  })
+  call.on('error', err => {
+    console.error('Client error: %s', err)
+  })
+  const widgets = [
+    { name: 'w1' },
+    new Error('Boom 1'),
+    { name: 'w2' },
+    { name: 'w3' },
+    { name: 'w4' },
+    new Error('Boom 1'),
+    { name: 'w5' }
+  ]
+
+  widgets.forEach(w => {
+    if (w instanceof Error) {
+      const { message } = w
+      call.write({ error: { message } })
+    } else {
+      call.write({ widget: w })
+    }
+  })
+  call.end()
+  fn()
+}
+
+function main () {
+  async.series([
+    getWidgetOK,
+    getWidgetError,
+    listWidgets,
+    listWidgetsError,
+    createWidgets,
+    createWidgetsError,
+    syncWidgets,
+    syncWidgetsError
+  ], () => {
+    console.log('done!')
+  })
+}
+
+main()
---
+++ errors/grpc_server.js
@@ -0,0 +1,84 @@
+const _ = require('lodash')
+const grpc = require('grpc')
+const path = require('path')
+
+const PROTO_PATH = path.resolve(__dirname, '../protos/errorexample.proto')
+const HOSTPORT = '0.0.0.0:50051'
+
+const loaded = grpc.load(PROTO_PATH)
+const example = loaded.ErrorExample
+
+const widgets = [
+  { name: 'w1' },
+  { name: 'w2' },
+  { name: 'w3' },
+  new Error('boom!'),
+  { name: 'w4' },
+  new Error('Another boom!'),
+  { name: 'w5' },
+  { name: 'w6' }
+]
+
+function listWidgets (call) {
+  _.each(widgets, w => {
+    if (w instanceof Error) {
+      const { message } = w
+      call.write({ error: { message } })
+    } else {
+      call.write({ widget: w })
+    }
+  })
+  call.end()
+}
+
+function getWidget (call, fn) {
+  const { id } = call.request
+  if (id && id % 2 === 0) {
+    return fn(new Error('boom!'))
+  }
+  fn(null, { name: `w${id}` })
+}
+
+function createWidgets (call, fn) {
+  let created = 0
+  call.on('data', d => created++)
+  call.on('end', () => {
+    if (created && created % 2 === 0) {
+      return fn(new Error('boom!'))
+    }
+    fn(null, { created })
+  })
+}
+
+function syncWidgets (call) {
+  let counter = 0
+  call.on('data', d => {
+    counter++
+    if (d.widget) {
+      console.log('data: %s', d.widget.name)
+      call.write({ widget: { name: d.widget.name.toUpperCase() } })
+    } else if (d.error) {
+      console.error('Error: %s', d.error.message)
+    }
+    if (counter % 4 === 0) {
+      call.write({ error: { message: `Boom ${counter}!` } })
+    }
+  })
+  call.on('end', () => {
+    call.end()
+  })
+}
+
+function main () {
+  const server = new grpc.Server()
+  server.addService(example.SampleService.service, {
+    getWidget,
+    listWidgets,
+    createWidgets,
+    syncWidgets
+  })
+  server.bind(HOSTPORT, grpc.ServerCredentials.createInsecure())
+  server.start()
+}
+
+main()
